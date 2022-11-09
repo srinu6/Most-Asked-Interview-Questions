e162dafddefcdeca15d145bc57d472f669941dd8
Srinu <srinumaripi.nitsri@gmail.com>
2022-11-09 10:37:18 +0530

    New questions

---
+++ ES6-letConst.js
@@ -0,0 +1,14 @@
+/* Most Important 
+     JavaScript ES6 Concepts! 
+     ➼ 1st ES6 ♛ Concept 
+     
+     Both let, const are
+     Block {} scoped.
+     */
+
+let myAge = 23;
+if (myAge > 18) {
+  let myName = "Rowdy Coder";
+  console.log(myName);
+}
+console.log(myName);
--- JS Coding/q1.js
+++ JS Coding/q1.js
@@ -63,3 +63,46 @@ const sum = (arr) => {
 };
 
 console.log(sum(arr));
+
+// Codepad Maripi Srinuvasarao|StanzaLiving|ReactNative SDE1|Coding
+
+/**
+ * Given an array [“123f”, “1dsa12”, “1212ds”, “65fd”, “sadfa”, “asdasd”]
+ * Each item can contain 0-9, a-z, A-Z where a-z, A-Z characters are unwanted
+ * Sum of all the numbers after removing all the unwanted characters 123+112+1212+65
+ **/
+
+const sumArr = (arr) => {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let str = "";
+
+    for (let j = 0; j < arr[i].length; j++) {
+      if (
+        arr[i].charAt(j) === "0" ||
+        arr[i].charAt(j) === "1" ||
+        arr[i].charAt(j) === "2" ||
+        arr[i].charAt(j) === "3" ||
+        arr[i].charAt(j) === "4" ||
+        arr[i].charAt(j) === "5" ||
+        arr[i].charAt(j) === "6" ||
+        arr[i].charAt(j) === "7" ||
+        arr[i].charAt(j) === "8" ||
+        arr[i].charAt(j) === "9"
+      ) {
+        str = str + arr[i].charAt(j);
+      }
+    }
+
+    if (str !== "") {
+      total = total + parseInt(str);
+    }
+  }
+  return total;
+};
+
+console.log(sumArr(["123f", "1dsa12", "1212ds", "65fd", "sadfa", "asdasd"])); // 1512
+console.log(sumArr(["asdasd"])); // 0
+console.log(sumArr(["asdasd12"])); // 12
+console.log(sumArr(["12"])); // 12
