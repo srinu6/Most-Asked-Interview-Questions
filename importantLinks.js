5cdce86b4ce1bbb97d988ff777d5764edf2ff9c3
Srinu <srinumaripi.nitsri@gmail.com>
2023-08-03 01:48:08 +0530

    links

---
+++ importantLinks.js
@@ -0,0 +1,4 @@
+// Hooks: https://react.dev/reference/react
+// useCallback, useMemo: https://www.knowledgehut.com/blog/web-development/all-about-react-usecallback#frequently-asked-questions
+
+//other concepts: https://react.dev/learn
