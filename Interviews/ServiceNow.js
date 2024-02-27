6a65cfe791154071628b1f4d12e19fe749e36f64
Srinu <srinumaripi.nitsri@gmail.com>
2024-02-27 20:24:07 +0530

    Service now

---
+++ Interviews/ServiceNow.js
@@ -0,0 +1,45 @@
+async function a() {
+  console.log("before a");
+  await b();
+  console.log("after a");
+}
+
+function b() {
+  console.log("before b");
+  const p = new Promise((resolve) => {
+    // Fix: Added a resolve function to the Promise constructor
+    setTimeout(() => {
+      console.log("in setTimeout");
+      resolve(); // Fix: Resolving the promise after the timeout
+    }, 1000);
+  });
+  console.log("after setTimeout");
+  return p;
+}
+
+console.log("first 1");
+a();
+console.log("second 1");
+
+// output
+
+// "first 1"
+// "before a"
+// "before b"
+// "after setTimeout"
+// "second 1"
+// "in setTimeout"
+// "after a"
+
+var a = 3;
+let b = 5;
+function c() {
+  let a = 7;
+  let b = 8;
+  console.log(a);
+  console.log(b);
+}
+c();
+
+// 7
+// 8
