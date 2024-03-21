be82dd7e497b2f2de80afacd29f694fcf859244c
srinuvasarao.maripi <srinuvasarao.maripi@pragmaticplay.com>
2024-03-21 11:41:47 +0530

    output question

--- JavaScript Output Questions/allInOne.js
+++ JavaScript Output Questions/allInOne.js
@@ -53,3 +53,74 @@ window.mySetTimeout(function () {
 }, 2000);
 
 ////////////////////////////////////////////////////////
+
+console.log("begins");
+
+setTimeout(() => {
+  console.log("setTimeout 1");
+  Promise.resolve().then(() => {
+    console.log("promise 1");
+  });
+}, 0);
+
+new Promise(function (resolve, reject) {
+  console.log("promise 2");
+  setTimeout(function () {
+    console.log("setTimeout 2");
+    resolve("resolve 1");
+  }, 0);
+}).then((res) => {
+  console.log("dot then 1");
+  setTimeout(() => {
+    console.log(res);
+  }, 0);
+});
+
+//output:
+("begins");
+("promise 2");
+("setTimeout 1");
+("promise 1");
+("setTimeout 2");
+("dot then 1");
+("resolve 1");
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+async function async1() {
+  console.log("async1 start");
+  await async2();
+  console.log("async1 end");
+}
+
+async function async2() {
+  console.log("async2");
+}
+
+console.log("script start");
+
+setTimeout(function () {
+  console.log("setTimeout");
+}, 0);
+
+async1();
+
+new Promise(function (resolve) {
+  console.log("promise1");
+  resolve();
+}).then(function () {
+  console.log("promise2");
+});
+
+console.log("script end");
+
+// output:
+
+("script start");
+("async1 start");
+("async2");
+("promise1");
+("script end");
+("async1 end");
+("promise2");
+("setTimeout");
